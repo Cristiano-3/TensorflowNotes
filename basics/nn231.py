b54e4e981ec2f9970a72063530e941bbaa8ab6b1
Cristiano-3 <chunanluo@126.com>
2019-05-16 09:51:29 +0800

    add bias

--- basics/nn231.py
+++ basics/nn231.py
@@ -13,30 +13,38 @@ Y = [[int(x1+x2 < 1)] for (x1, x2) in X]
 
 # batch size
 batch_size = 8
+# ----------------------------------------
 
+# Step:1
 # 定义神经网络参数, 
 w1 = tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1), name='w1')
 w2 = tf.Variable(tf.random_normal([3, 1], stddev=1, seed=1), name='w2')
+bias1 = tf.Variable(tf.zeros([1]))  # important!!  
+bias2 = tf.Variable(tf.zeros([1]))  # without bias can't learn a right model
 
 # 定义输入
 x = tf.placeholder(tf.float32, shape=[None, 2], name='x-input')
 y_ = tf.placeholder(tf.float32, shape=[None, 1], name='y-input')
 
 # forward
-a = tf.matmul(x, w1)
-y = tf.matmul(a, w2)
+a = tf.add(tf.matmul(x, w1), bias1)
+y = tf.add(tf.matmul(a, w2), bias2)
 
 # output sigmoid
 y = tf.sigmoid(y)
 
-# cross-entropy loss
-loss = tf.reduce_mean(-(y_ * tf.log(y) + (1-y_) * tf.log(1-y)))
 
+# Step:2
+# cross-entropy loss
+loss = tf.reduce_mean(-(y_ * tf.log(tf.clip_by_value(y, 1e-10, 1.0)) + (1-y_) * tf.log(tf.clip_by_value((1-y), 1e-10, 1.0))))
+
+# optimize
 optimizer = tf.train.GradientDescentOptimizer(0.001)
 train_op = optimizer.minimize(loss)
 
-init_op = tf.global_variables_initializer()
 
+# Step:3
+init_op = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init_op)
 
@@ -44,7 +52,7 @@ with tf.Session() as sess:
     print(sess.run(w1))
     print(sess.run(w2))
 
-    STEPS = 10000
+    STEPS = 20000
     for i in range(STEPS):
         # get batch
         start = (i * batch_size) % dataset_size
@@ -55,20 +63,25 @@ with tf.Session() as sess:
         sess.run(train_op, feed_dict={x:x_batch, y_:y_batch})
 
         # print infos, such as: loss ...
-        if i % 1000 == 0:
+        if i % 10000 == 0:
             total_cross_entropy = sess.run(loss, feed_dict={x:X, y_:Y})
             print("After %d training steps, cross entropy on all data is %g" % (i, total_cross_entropy))
 
     print('weights after training:')
-    w1_res = sess.run(w1)
-    w2_res = sess.run(w2)
-    print(w1_res)
-    print(w2_res)
+    print(sess.run(w1))
+    print(sess.run(w2))
 
     for i in range(dataset_size):
         if Y[i][0] == 1:
             plt.scatter(X[i,0], X[i,1], c='b', marker='o')
         else:
             plt.scatter(X[i,0], X[i,1], c='r', marker='o')
+
+    Y_res = sess.run(y, feed_dict={x:X})
+    for i in range(dataset_size):
+        if Y_res[i][0] >= 0.5:
+            plt.scatter(X[i,0], X[i,1], c='b', marker='x')
+        else:
+            plt.scatter(X[i,0], X[i,1], c='r', marker='x')
 
     plt.show()
